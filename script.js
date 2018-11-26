cc7188c4f9bd59432f8972f403fae8e396dec9b4
Lizzie Gasson <lizzie.gasson@iag.co.nz>
2018-11-27 12:22:49 +1300

    added some jquery to my script file and set height of header to fill view height

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function displayMenu(){
-    console.log("Menu clicked");
-}
 
 function scrollFunc(elementId){
     var element = document.getElementById(elementId);
     element.scrollIntoView(true);
 }
+
+$(function() {
+
+    var myButton = $('.btn.btn-arrow')
+
+    $('#hamburger').on('click', function(){
+        $(this).toggleClass('active');
+        myButton.fadeOut( "slow", function() {
+            // Animation complete.
+          });
+    });
+
+});
