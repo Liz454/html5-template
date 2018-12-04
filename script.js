a1c2e0313a88a1adae0b1e8121d90b134fc2f2c2
Lizzie Gasson <lizzie.gasson@iag.co.nz>
2018-12-05 12:21:48 +1300

    changes to header and hamburger animation

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ function scrollFunc(elementId){
 }
 
 $(function() {
-
-    var myButton = $('.btn.btn-arrow')
-
     $('#hamburger').on('click', function(){
-        $(this).toggleClass('active');
-        myButton.fadeOut( "slow", function() {
-            // Animation complete.
-          });
+        // var bar1 = $("#hb-bar1");
+        // var bar2 = $("#hb-bar2");
+        // var bar3 = $("#hb-bar3");
+        // if (bar1.hasClass('bar1-animate') || bar1.hasClass('bar1-animate-reverse')){
+        //     bar1.toggleClass("bar1-animate bar1-animate-reverse");
+        // }else{
+        //     bar1.addClass("bar1-animate");
+        // }
+        $("#hb-bar1").toggleClass("bar1-animate");
+        $("#hb-bar2").toggleClass("bar2-animate");
+        $("#hb-bar3").toggleClass("bar3-animate");
     });
-
 });
